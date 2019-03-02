7a03965ef3f1c2e0765b3471dc7a8ebf5cece5c0
Jainish <jainishbhuva@gmail.com>
2019-03-02 16:02:08 +0530

    Register controller(Create) change

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,10 +66,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'education' => $data['education'],
-            'location' => $data['location'],
-            'notes' => $data['notes'],
-            'images' => $data['images'],
             'password' => Hash::make($data['password']),
         ]);
     }
